92f794ecea50b1db5c258dbc2ee5187cf144696c
00013540 <wiut00013540@gmail.com>
2024-10-29 22:08:34 +0500

    Book's controller added

---
+++ DSCC_13540_MVC/Controllers/BookController.cs
@@ -0,0 +1,171 @@
+﻿using DSCC_13540_MVC.DTOs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Net.Http.Headers;
+using System.Text;
+
+namespace DSCC_13540_MVC.Controllers
+{
+    public class BookController : Controller
+    {
+        private readonly string BaseUrl = "http://localhost:7137/";
+
+        // GET: BookController
+        public async Task<ActionResult> Index()
+        {
+            List<BookDto> books = new List<BookDto>();
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(BaseUrl);
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                HttpResponseMessage response = await client.GetAsync("api/book/GetAllBooks");
+                if (response.IsSuccessStatusCode)
+                {
+                    var responseContent = await response.Content.ReadAsStringAsync();
+                    books = JsonConvert.DeserializeObject<List<BookDto>>(responseContent);
+                }
+            }
+            return View(books);
+        }
+
+        // GET: BookController/Details/5
+        public async Task<ActionResult> Details(int id)
+        {
+            BookDto book = new BookDto();
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(BaseUrl);
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                var response = await client.GetAsync($"api/book/GetBookById/{id}");
+                if (response.IsSuccessStatusCode)
+                {
+                    var responseContent = await response.Content.ReadAsStringAsync();
+                    book = JsonConvert.DeserializeObject<BookDto>(responseContent);
+                }
+            }
+            return View(book);
+        }
+
+        // GET: BookController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: BookController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create(BookCreateDto book)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(BaseUrl);
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                var jsonContent = JsonConvert.SerializeObject(book);
+                var contentString = new StringContent(jsonContent, Encoding.UTF8, "application/json");
+                var response = await client.PostAsync("api/book/CreateBook", contentString);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+            }
+            return View(book);
+        }
+
+        // GET: BookController/Edit/5
+        public async Task<ActionResult> Edit(int id)
+        {
+            BookDto book = new BookDto();
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(BaseUrl);
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                var response = await client.GetAsync($"api/book/GetBookById/{id}");
+                if (response.IsSuccessStatusCode)
+                {
+                    var responseContent = await response.Content.ReadAsStringAsync();
+                    book = JsonConvert.DeserializeObject<BookDto>(responseContent);
+                }
+            }
+            return View(book);
+        }
+
+        // POST: BookController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit(int id, BookCreateDto bookUpdateDto)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(BaseUrl);
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                var jsonContent = JsonConvert.SerializeObject(bookUpdateDto);
+                var contentString = new StringContent(jsonContent, Encoding.UTF8, "application/json");
+                var response = await client.PutAsync($"api/book/UpdateBook/{id}", contentString);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+            }
+            return View(bookUpdateDto);
+        }
+
+        // GET: BookController/Delete/5
+        public async Task<ActionResult> Delete(int id)
+        {
+            BookDto book = new BookDto();
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(BaseUrl);
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                var response = await client.GetAsync($"api/book/GetBookById/{id}");
+                if (response.IsSuccessStatusCode)
+                {
+                    var responseContent = await response.Content.ReadAsStringAsync();
+                    book = JsonConvert.DeserializeObject<BookDto>(responseContent);
+                }
+            }
+            return View(book);
+        }
+
+        // POST: BookController/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(BaseUrl);
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                var response = await client.DeleteAsync($"api/book/DeleteBook/{id}");
+
+                if (response.IsSuccessStatusCode)
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+            }
+            return View();
+        }
+    }
+}
